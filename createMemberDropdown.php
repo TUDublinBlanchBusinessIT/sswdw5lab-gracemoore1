28d2b76bdf022e1205791506dffe254954d8082b
B00153274 Grace Moore <B00153274@mytudublin.ie>
2024-10-15 15:35:40 +0100

    dropdown commit

---
+++ createMemberDropdown.php
@@ -0,0 +1,31 @@
+<?php 
+include("dbcon.php");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tennisclub";
+$port = 3307;
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * from member";
+$result = mysqli_query($conn, $sql);
+
+echo "<SELECT name='memberID'>";
+
+while($row = mysqli_fetch_assoc($result)) {
+    $id=$row['id'];
+    $fn=$row['firstname'];
+    $sn=$row['surname'];
+    echo "<OPTION value>$id=$fn $sn</OPTION>";
+}
+echo "</SELECT>";
+
+mysqli_close($conn); 
+?>
